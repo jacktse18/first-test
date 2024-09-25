4c34e547e9f5db560cf7d942c6521faa95367a47
Jack Tse <jacktse18@hotmail.com>
2024-09-25 11:16:47 -0700

    first push of public info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/first-test",
   siteMetadata: {
     title: `My Test Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
